25667c9d76753619737c9f4143e6f56363c95d86
bb0820421 <166367330+bb0820421@users.noreply.github.com>
2024-04-29 15:13:19 +0800

    Add files via upload

---
+++ pen-export-zYXyqZN/README.md
@@ -0,0 +1,4 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/201-11-/pen/zYXyqZN](https://codepen.io/201-11-/pen/zYXyqZN).
+
---
+++ pen-export-zYXyqZN/dist/script.js
@@ -0,0 +1,15 @@
+function calculateRelativeHumidity() {
+      var temperature = parseFloat(document.getElementById("temperature").value);
+      var dewPoint = parseFloat(document.getElementById("dewPoint").value);
+      
+      if (isNaN(temperature) || isNaN(dewPoint)) {
+        document.getElementById("result").value = "請輸入有效數字";
+        return;
+      }
+
+      var relativeHumidity = Math.round(
+        100 * (Math.exp((17.625 * dewPoint) / (243.04 + dewPoint)) / Math.exp((17.625 * temperature) / (243.04 + temperature)))
+      );
+
+      document.getElementById("result").value = relativeHumidity + "%";
+    }
